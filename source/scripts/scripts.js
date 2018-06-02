7111676927358b765db83a5f49c318ef61af74b7
Romuald <robindranat@gmail.com>
2018-06-02 16:18:14 +0300

    Прогресс - форма отправки

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 		}		
 		
 		var imgSize = ($('.partners-block__img').width());
-	
+
 
 		if ($(window).width() >= imgSize) {
 			$('.partners-block__img').removeClass('active');
@@ -42,14 +42,14 @@ $(document).ready(function() {
 		$(".filter-az__list, .filter-az__list-big").empty();
 		$('.filter-az__list-big').html(LetterID);
 		$(function(){
-		    $.getJSON('../scripts/list.json', function(data) {
-		    	for(var i=0; i<data.length; i++){
-		    		var valueField = data[i].name[0];
-		    		if (valueField == LetterID) {
-		    			$('.filter-az__list').append('<li>'  + data[i].name + '</li>');
-		    		}
-		    	}  
-		    });
+			$.getJSON('../scripts/list.json', function(data) {
+				for(var i=0; i<data.length; i++){
+					var valueField = data[i].name[0];
+					if (valueField == LetterID) {
+						$('.filter-az__list').append('<li>'  + data[i].name + '</li>');
+					}
+				}  
+			});
 		});
 		var destination = $('.content_letter').offset().top;
 		$('html, body').animate({ scrollTop: destination }, 1100);
@@ -73,5 +73,86 @@ $(document).ready(function() {
 			$('.filter-az_digits').addClass('active');
 		}
 	});
+
+
+	// селект
+	function initCustomSelectControls() {
+		$('.k-select__item.active').each(function() {
+			var $container = $(this).closest(".k-select");
+			$container.find('.k-select__input').val($(this).text());
+		});
+
+		$('.k-select .mdi-chevron-down, .k-select__input').click(function() {
+			var $container = $(this).closest('.k-select');
+
+
+			if(!$container.hasClass("active"))
+			{
+				$('.k-select.active, .k-select__dropbox.active').removeClass('active');
+			}
+
+			$container.find('.k-select__dropbox').toggleClass('active');
+			$container.toggleClass('active');
+		});
+		
+		$('.k-select__item').click(function() {
+			var $container = $(this).closest(".k-select");
+			if ($(this).hasClass('active')) {
+				$container.find('.k-select__item').removeClass('active');
+			}
+			else {
+				$container.find('.k-select__item').removeClass('active');
+				$(this).addClass('active');
+			}
+			
+			var secletedText = $container.find('.k-select__item.active').text();
+			$container.find('.k-select__input').val(secletedText);
+
+			
+
+			//выводим значение выбраного элемента в скрытый блок
+			var selectID = $(this).closest('.k-select').attr('id');
+			if (selectID == 's1') {
+				$('#hidden-1').val(secletedText);
+			}
+			else {
+				$('#hidden-2').val(secletedText);
+			}
+			
+
+
+			$container.find('.k-select__dropbox').removeClass('active');
+			$container.removeClass('active');
+
+			
+			
+		});
+	}
+	
+
+	initCustomSelectControls();
+
+
+	//выбор подслелекта
+	$('#s1').click(function() {
+		$('#s1 .k-select__item').each(function(i,elem) {
+			if ($(this).hasClass('active')) {
+
+				$('.form-block__services .k-select').each(function(n,elem) {
+
+					if (n == i) {
+						$(this).addClass('visible');
+					}
+					else {
+						$(this).removeClass('visible');
+					}
+				});
+			}
+		});
+	});
+
+
+
+
 	
 });
